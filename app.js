bec78899ac95703af0ae8213f2c10ec858ac52eb
HighClassGrey <tamekaahannah@gmail.com>
2018-09-26 19:19:37 -0500

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+let friends = ['Marrea', 'Tavanya', 'Sonja', 'Debra', 'Mark'];
+for (let i= 0; i<friends.length; i++)
+    console.log(friends[i]);
+for (let num=99; num >0; num--){
+    console.log( num 'lines of code in the file', num 'lines of code', friends[i] 'strikes out, clears it all out', num-1 'lines of code in the file');
+}
