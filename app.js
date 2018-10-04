e810313683aee3048d0746689cc0395b65f4b949
HighClassGrey <tamekaahannah@gmail.com>
2018-10-04 11:45:31 -0500

    final

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ for (let i = 0; i < friends.length; i++) {
     //First console.log works
     var num = 99;
     while (num > 0) {
-        console.log(num, 'lines of code in the file', num, 'lines of code', friends[i], 'strikes out, clears it all out', num - 1, 'lines of code in the file');
+        console.log(num, 'lines of code in the file,', num, 'lines of code,', friends[i], 
+        'strikes out, clears it all out', num - 1, 'lines of code in the file!');
         num--;
     }
 }
