ff892a4f40ed9fdd5c3b34a8ced93fb6f37de99b
HighClassGrey <tamekaahannah@gmail.com>
2018-10-04 11:32:30 -0500

    final

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let friends = ['Marrea', 'Tavanya', 'Sonja', 'Debra', 'Mark'];
-for (let i = 0; i < friends.length; i++)
+for (let i = 0; i < friends.length; i++) {
     console.log(friends[i]);
-//First console.log works
-var num = 99;
-while (num > 0) {
-    console.log( num, 'lines of code in the file', num, 'lines of code', friends[0],'strikes out, clears it all out', num-1,'lines of code in the file');
-    num--;
+    //First console.log works
+    var num = 99;
+    while (num > 0) {
+        console.log(num, 'lines of code in the file', num, 'lines of code', friends[i], 'strikes out, clears it all out', num - 1, 'lines of code in the file');
+        num--;
+    }
 }
